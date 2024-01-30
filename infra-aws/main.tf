8d2f8252b078d5580f381242a13b5a04bada81bb
bazilbrush <bazileuski.jack@gmail.com>
2024-01-30 15:06:45 +0000

    added env secrets

--- infra-aws/main.tf
+++ infra-aws/main.tf
@@ -28,4 +28,9 @@ provider "azurerm" {
 }
 provider "aws" {
   region = "eu-west-1"
+#   assume_role_with_web_identity {
+#     role_arn                = "arn:aws:iam::123456789012:role/ROLE_NAME"
+#     session_name            = "SESSION_NAME"
+#     web_identity_token_file = "/Users/tf_user/secrets/web-identity-token"
+#   }
 }
