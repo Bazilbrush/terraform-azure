9dcf5b7858608c04c39f94c3733c36a87d1430ce
bazilbrush <bazileuski.jack@gmail.com>
2024-01-31 08:59:46 +0000

    testing with toiken file

--- infra-aws/main.tf
+++ infra-aws/main.tf
@@ -28,9 +28,9 @@ provider "azurerm" {
 }
 provider "aws" {
   region = "eu-west-1"
-#   assume_role_with_web_identity {
-#     role_arn                = "arn:aws:iam::123456789012:role/ROLE_NAME"
-#     session_name            = "SESSION_NAME"
-#     web_identity_token_file = "/Users/tf_user/secrets/web-identity-token"
-#   }
+  assume_role_with_web_identity {
+    role_arn                = "arn:aws:iam::339713169581:role/github-oidc"
+    session_name            = "github-test"
+    web_identity_token_file = "/tmp/web_identity_token_file"
+  }
 }
